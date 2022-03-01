dfdc82f3cdd32606037e2a02d80c46bad5897a2a
Hajime Nakagawa <51704330+HajimexxxNakagawa@users.noreply.github.com>
2022-03-01 18:46:33 +0900

    Initial commit

---
+++ .storybook/preview.js
@@ -0,0 +1,44 @@
+import "../src/styles/globals.css"
+import * as NextImage from "next/image"
+import { addDecorator } from "@storybook/react"
+import { withConsole } from "@storybook/addon-console"
+import { initialize, mswDecorator } from "msw-storybook-addon"
+
+// Initialize MSW
+initialize()
+
+addDecorator((storyFn, context) => withConsole()(storyFn)(context))
+
+const OriginalNextImage = NextImage.default
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+})
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  previewTabs: {
+    "storybook/docs/panel": { index: -1 },
+  },
+  msw: {
+    handlers: [
+      // example
+      rest.get("/hello", (req, res, ctx) => {
+        return res(
+          ctx.json({
+            name: "John Doe",
+          })
+        )
+      }),
+    ],
+  },
+}
+
+export const decorators = [mswDecorator]
